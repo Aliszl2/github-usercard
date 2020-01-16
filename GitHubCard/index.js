9218ff9dcc6ce93451daf760fee907121cca4aa2
Alison Law <alisparklaw@gmail.com>
2020-01-16 11:52:16 +0000

    html for cards

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -42,25 +42,71 @@ axios
 
 const followersArray = [];
 
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
-
-<div class="card">
-  <img src={image url of user} />
-  <div class="card-info">
-    <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
-    <p>Profile:  
+// Step 3: Create a function that accepts a single object as its only argument,
+//    Using DOM methods and properties, create a component that will return the following DOM element:
+{
+  /* <div class="card">
+      <img src={image url of user} />
+      <div class="card-info">
+      <h3 class="name">{users name}</h3>
+      <p class="username">{users user name}</p>
+      <p>Location: {users location}</p>
+      <p>Profile:  
       <a href={address to users github page}>{address to users github page}</a>
-    </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
-  </div>
-</div>
+      </p>
+      <p>Followers: {users followers count}</p>
+      <p>Following: {users following count}</p>
+      <p>Bio: {users bio}</p>
+      </div>
+    </div> */
+}
 
-*/
+function createCard(obj) {
+  //HTML
+  const card = document.createElement("div");
+
+  const img = document.createElement("img");
+  const info = document.createElement("div");
+  const name = document.createElement("h3");
+  const userName = document.createElement("p");
+  const location = document.createElement("p");
+  const profile = document.createElement("p");
+  const gitHubLink = document.createElement("a");
+  const followers = document.createElement("p");
+  const following = document.createElement("p");
+  const bio = document.createElement("p");
+  //Class Names
+  card.classList.add("card");
+  info.classList.add("card-info");
+  name.classList.add("name");
+  userName.classList.add("username");
+  //Content
+  name.textContent = obj.data.name;
+  img.src = obj.data.avatar_url;
+  userName.textContent = obj.data.login;
+  location.textContent = `Location: ${obj.data.location}`;
+  profile.textContent = `Profile: `;
+  gitHubLink.setAttribute("href", obj.data.html_url);
+  gitHubLink.textContent = obj.data.html_url;
+  followers.textContent = `Followers ${obj.data.followers}`;
+  following.textContent = `Following ${obj.data.following}`;
+  bio.textContent = `Bio: ${obj.data.bio}`;
+  //HTML structure
+  card.append(img);
+  card.append(info);
+  
+  info.append(name);
+  info.append(userName);
+  info.append(location);
+  info.append(profile);
+  info.append(followers);
+  info.append(following);
+  info.append(bio);
+  
+  profile.append(gitHubLink);
+  
+  return card;
+}
 
 /* List of LS Instructors Github username's: 
   tetondan
