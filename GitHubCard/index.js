2c94f424c025a7b62c100d1e8133d88cc523ba26
Alison Law <alisparklaw@gmail.com>
2020-01-16 12:18:20 +0000

    cards created from given instructors

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,36 +1,12 @@
-/* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
-*/ axios
+ axios
   .get("https://api.github.com/users/Aliszl")
   .then(response => {
-    console.log(response);
-    console.log(response.data);
     let newCard = document.querySelector(".cards");
-    newCard.append(createCard(response))
+    newCard.append(createCard(response));
   })
   .catch(error => {
     console.log(error);
   });
-axios
-  .get("https://api.github.com/users/Aliszl/followers")
-  .then(response => {
-    console.log(response);
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.log(error);
-  });
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
-
-   Skip to Step 3.
-*/
-
-/* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
-*/
 
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
@@ -41,9 +17,35 @@ axios
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
-
-const followersArray = [];
-
+const followersArray = [
+  "tetondan",
+  "dustinmyers",
+  "justsml",
+  "luishrd",
+  "bigknell"];
+followersArray.forEach((follower)=>{
+  console.log(follower);
+  axios
+  .get(`https://api.github.com/users/${follower}`)
+  .then(response => {
+    console.log(response);
+    console.log(response.data);
+    let newCard = document.querySelector(".cards");
+    newCard.prepend(createCard(response));
+  })
+  .catch(error => {
+    console.log(error);
+  });
+})
+axios
+  .get("https://api.github.com/users/Aliszl/followers")
+  .then(response => {
+    console.log(response);
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
 // Step 3: Create a function that accepts a single object as its only argument,
 //    Using DOM methods and properties, create a component that will return the following DOM element:
 {
@@ -62,7 +64,6 @@ const followersArray = [];
       </div>
     </div> */
 }
-
 function createCard(obj) {
   //HTML
   const card = document.createElement("div");
@@ -96,7 +97,7 @@ function createCard(obj) {
   //HTML structure
   card.append(img);
   card.append(info);
-  
+
   info.append(name);
   info.append(userName);
   info.append(location);
@@ -104,9 +105,9 @@ function createCard(obj) {
   info.append(followers);
   info.append(following);
   info.append(bio);
-  
+
   profile.append(gitHubLink);
-  
+
   return card;
 }
 
